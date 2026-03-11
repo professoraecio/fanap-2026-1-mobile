a18b5a14e5ef42ff86a5831e4f8f4e747cd950a9
Professor Aécio <professoraecio@gmail.com>
2026-03-11 20:51:27 -0300

    add

--- ExemploUtilSharedPreferences/app/src/main/java/com/professoraecio/exemploutilsharedpreferences/util/UtilSharedPreferences.java
+++ ExemploUtilSharedPreferences/app/src/main/java/com/professoraecio/exemploutilsharedpreferences/util/UtilSharedPreferences.java
@@ -1,11 +1,25 @@
 package com.professoraecio.exemploutilsharedpreferences.util;
 
+import android.content.Context;
+import android.content.SharedPreferences;
+
 public class UtilSharedPreferences {
 
     // Definindo o nome do banco de dados a salvar os dados
     private static final String PREFERENCE_NAME = "MyPreferences";
 
     // Salvar o dado (String)
+    public static void saveString(Context context,
+                                  String key,
+                                  String value){
+        SharedPreferences
+                sharedPreferences =
+                context.getSharedPreferences(PREFERENCE_NAME,
+                        Context.MODE_PRIVATE);
+        SharedPreferences.Editor editor = sharedPreferences.edit();
+        editor.putString(key,value);
+        editor.apply(); // salvando dados...
+    }
 
     // Recuperar o dado (String)
 
