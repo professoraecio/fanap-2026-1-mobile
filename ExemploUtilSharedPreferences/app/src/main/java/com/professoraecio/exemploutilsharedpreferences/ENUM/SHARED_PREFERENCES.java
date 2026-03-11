c162e09ce7c38ce5d1872b68a8ec39c1663fc92a
Professor Aécio <professoraecio@gmail.com>
2026-03-11 20:59:44 -0300

    add

--- ExemploUtilSharedPreferences/app/src/main/java/com/professoraecio/exemploutilsharedpreferences/ENUM/SHARED_PREFERENCES.java
+++ ExemploUtilSharedPreferences/app/src/main/java/com/professoraecio/exemploutilsharedpreferences/ENUM/SHARED_PREFERENCES.java
@@ -1,4 +1,25 @@
 package com.professoraecio.exemploutilsharedpreferences.ENUM;
 
 public enum SHARED_PREFERENCES {
+
+    // Contém todas as chaves de acesso
+    // ao cofre de dados
+    // exemplos:
+    // usuario
+    // senha
+    // residencia
+    // local trabalho...
+    // USUARIO_KEY , SENHA_KEY , RESIDENCIA_KEY
+
+    USUARIO_KEY("USUARIO_KEY"),
+    SENHA_KEY("SENHA_KEY"),
+    RESIDENCIA_KEY("RESIDENCIA_KEY");
+
+    // KEY e a mesma coisa que chave...
+
+    public String chave;
+
+    SHARED_PREFERENCES(String chave){
+        this.chave = chave;
+    }
 }
