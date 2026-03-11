8576d6226b0996791dfce6546af909dedbe45a0b
Professor Aécio <professoraecio@gmail.com>
2026-03-11 20:48:26 -0300

    add

--- ExemploUtilSharedPreferences/app/src/main/java/com/professoraecio/exemploutilsharedpreferences/util/UtilSharedPreferences.java
+++ ExemploUtilSharedPreferences/app/src/main/java/com/professoraecio/exemploutilsharedpreferences/util/UtilSharedPreferences.java
@@ -1,4 +1,12 @@
 package com.professoraecio.exemploutilsharedpreferences.util;
 
 public class UtilSharedPreferences {
+
+    // Definindo o nome do banco de dados a salvar os dados
+    private static final String PREFERENCE_NAME = "MyPreferences";
+
+    // Salvar o dado (String)
+
+    // Recuperar o dado (String)
+
 }
