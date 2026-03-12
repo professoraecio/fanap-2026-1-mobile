5692e27835b1439c35a3da3d4ec3dd6843b4c3e2
Professor Aécio <professoraecio@gmail.com>
2026-03-11 21:06:18 -0300

    add

--- ExemploUtilSharedPreferences/app/src/main/java/com/professoraecio/exemploutilsharedpreferences/ENUM/DEFAULT_VALUES.java
+++ ExemploUtilSharedPreferences/app/src/main/java/com/professoraecio/exemploutilsharedpreferences/ENUM/DEFAULT_VALUES.java
@@ -1,4 +1,20 @@
 package com.professoraecio.exemploutilsharedpreferences.ENUM;
 
 public enum DEFAULT_VALUES {
+
+    // Caso o dado que for acessado com a chave (key)
+    // esteja vazio (nenhum dado foi cadastrado antes)
+    // neste caso eu retorno o valor de fábrica
+    // também conhecido com default value
+
+    USUARIO_DEFAULT_VALUE("admin"),
+    SENHA_DEFAULT_VALUE("admin"),
+    RESIDENCIA_DEFAULT_VALUE("home");
+
+    public String valor;
+
+    DEFAULT_VALUES(String valor){
+        this.valor = valor;
+    }
+
 }
---
+++ ExemploUtilSharedPreferences/app/src/main/java/com/professoraecio/exemploutilsharedpreferences/persistencia/Persistencia.java
@@ -0,0 +1,4 @@
+package com.professoraecio.exemploutilsharedpreferences.persistencia;
+
+public class Persistencia {
+}
