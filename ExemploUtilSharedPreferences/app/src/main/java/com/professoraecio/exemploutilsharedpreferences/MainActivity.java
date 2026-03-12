a5d971fe56ba182ecea157c75b5466f348719c16
Professor Aécio <professoraecio@gmail.com>
2026-03-11 21:11:43 -0300

    Fim projeto

--- ExemploUtilSharedPreferences/app/src/main/java/com/professoraecio/exemploutilsharedpreferences/MainActivity.java
+++ ExemploUtilSharedPreferences/app/src/main/java/com/professoraecio/exemploutilsharedpreferences/MainActivity.java
@@ -4,11 +4,20 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.professoraecio.exemploutilsharedpreferences.persistencia.Persistencia;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Salvar o usuario
+        Persistencia.salvarUsuario(this,"jose");
+
+        // Carregar o usuario
+        String usuario = Persistencia.lerUsuario(this);
+
     }
 }
