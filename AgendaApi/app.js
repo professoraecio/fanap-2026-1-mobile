f3c4178a90e90d3fac462b3f3758f87278acfb60
Professor Aécio <professoraecio@gmail.com>
2026-04-22 20:34:14 -0300

    Iniciar api rest agenda

---
+++ AgendaApi/app.js
@@ -0,0 +1,27 @@
+const URL_BASE = '/api.agenda';
+
+const express = require('express');
+const app = express();
+
+const morgan = require('morgan');
+app.use(morgan('dev'));
+
+const bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({extended : false}));
+app.use(bodyParser.json());
+
+// Config rota principal
+app.use(URL_BASE,(req,res,next) => {
+    res.status(200).send({
+        resposta : 'URL do app funcionou'
+    });
+});
+
+// Rota não encontrada
+app.use((req,res,next) => {
+    const erro = new Error('Rota inexistesnte');
+    erro.status = 404;
+    next(erro);
+});
+
+module.exports = app;
