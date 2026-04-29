3644f45e74a077c5e0d8ec0fd31846f3da88f0af
Professor Aécio <professoraecio@gmail.com>
2026-04-29 19:39:00 -0300

    Implementar rota get id

--- AgendaApi/rotas/contatoDao.js
+++ AgendaApi/rotas/contatoDao.js
@@ -4,30 +4,60 @@ const url = require('url'); // ter acesso a url
 const queryString = require('querystring'); // fazer query (filtro) na url
 const mysql = require('./mysql').pool;
 
-router.post('/create',(req,res,next) => {
-    const {nome,fone,email} = req.body
-    const contato = {nome,fone,email} 
-    mysql.getConnection((error,conn)=>{
-        if(error){
+router.post('/create', (req, res, next) => {
+    const { nome, fone, email } = req.body
+    const contato = { nome, fone, email }
+    mysql.getConnection((error, conn) => {
+        if (error) {
             return res.status(500).send({
-                error : error,
-                response : null
+                error: error,
+                response: null
             });
         }
         conn.query(
             'INSERT INTO contato (nome,fone,email) VALUES (?,?,?)',
-            [contato.nome,contato.fone,contato.email],
-            (error,resultado,field) => {
+            [contato.nome, contato.fone, contato.email],
+            (error, resultado, field) => {
                 conn.release();
-                if(error){
+                if (error) {
                     return res.status(500).send({
-                        error : error,
-                        response : null
+                        error: error,
+                        response: null
                     });
                 }
                 res.status(201).send({
-                    response : 'Contato cadastrado com sucesso',
+                    response: 'Contato cadastrado com sucesso',
                     id_contato: resultado.insertId
+                });
+            }
+        )
+    });
+});
+
+// http://localhost:3000/api.agenda/contato-dao/getId?id=1
+router.get('/getId', (req, res, next) => {
+    const reqUrl = url.parse(req.url);
+    const queryParams = queryString.parse(reqUrl.query);
+    const paramId = queryParams.id;
+    mysql.getConnection((error, conn) => {
+        if (error) {
+            return res.status(500).send({
+                error: error,
+                response: null
+            });
+        }
+        conn.query(
+            'SELECT * FROM contato WHERE id = ?;',
+            [paramId],
+            (error, resultado, fields) => {
+                if (error) {
+                    return res.status(500).send({
+                        error: error,
+                        response: null
+                    });
+                }
+                return res.status(200).send({
+                    response : resultado
                 });
             }
         )
